2e3fa72f5beb7baed354ad81c9f284a3dab2b939
James McCarthy <jamesmccarthyxci@gmail.com>
2019-12-03 16:14:19 -0500

    Removed helment

--- routes/api/game.js
+++ routes/api/game.js
@@ -1,61 +1,58 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const axios = require('axios');
+const auth = require("../../middleware/auth");
+const axios = require("axios");
 
 // const Profile = require('../../models/Profile');
 // const User = require('../../models/User');
 
-router.get('/get-random-questions', async (req, res) => {
-    try {
-        const jservice_res = await axios({
-            method: 'get',
-            url: 'http://jservice.io/api/random',
-            params: {
-                count: 100
-            }
-        });
+router.get("/get-random-questions", async (req, res) => {
+  try {
+    const jservice_res = await axios({
+      method: "get",
+      url: "http://jservice.io/api/random",
+      params: {
+        count: 100
+      }
+    });
 
-        const clues = JSON.stringify(jservice_res, getCircularReplacer());
+    const clues = JSON.stringify(jservice_res, getCircularReplacer());
 
-        res.send(JSON.stringify(clues));
-
-    } catch (error) {
-        console.log(error.message)
-    }
+    res.send(JSON.stringify(clues));
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
-router.get('/get-question', async (req, res) => {
-    try {
+router.get("/get-question", async (req, res) => {
+  try {
+    const jservice_res = await axios({
+      method: "get",
+      url: "http://jservice.io/api/clues",
+      params: {
+        value: req.query.value,
+        category: req.query.category_id
+      }
+    });
 
-        const jservice_res = await axios({
-            method: 'get',
-            url: 'http://jservice.io/api/clues',
-            params: {
-                value: req.query.value,
-                category: req.query.category_id
-            }
-        });
-
-        res.send(jservice_res.data);
-
-    } catch (error) {
-        console.log(error.message)
-    }
+    res.send(jservice_res.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Cyclic_object_value
 const getCircularReplacer = () => {
-    const seen = new WeakSet();
-    return (key, value) => {
-        if (typeof value === "object" && value !== null) {
-            if (seen.has(value)) {
-                return;
-            }
-            seen.add(value);
-        }
-        return value;
-    };
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
 };
 
 module.exports = router;
